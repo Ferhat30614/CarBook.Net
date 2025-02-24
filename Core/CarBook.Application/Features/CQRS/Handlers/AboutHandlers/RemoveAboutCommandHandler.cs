0dd8afbb120fced3f457b68569dcde5af662fe9d
Ferhat30614 <149321307+Ferhat30614@users.noreply.github.com>
2025-02-24 16:33:10 +0400

    update create işlemleri tamam sıra removeda

---
+++ Core/CarBook.Application/Features/CQRS/Handlers/AboutHandlers/RemoveAboutCommandHandler.cs
@@ -0,0 +1,20 @@
+﻿using CarBook.Application.Interfaces;
+using CarBook.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Application.Features.CQRS.Handlers.AboutHandlers
+{
+    public class RemoveAboutCommandHandler
+    {
+        private readonly IRepository<About> _repository;
+
+        public RemoveAboutCommandHandler(IRepository<About> repository)
+        {
+            _repository = repository;
+        }
+    }
+}
