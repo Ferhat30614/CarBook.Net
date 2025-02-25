925a6922ef9e3617e81f5b04dcedad3b1d84869d
Ferhat30614 <149321307+Ferhat30614@users.noreply.github.com>
2025-02-25 14:56:15 +0400

    Car tablosu için aynı işlemleri yapmaya bnaşladım

---
+++ Core/CarBook.Application/Features/CQRS/Queries/CatQueries/GetCarByIdQuery.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Application.Features.CQRS.Queries.CatQueries
+{
+    public class GetCarByIdQuery
+    {
+
+        public int Id { get; set; }
+
+        public GetCarByIdQuery(int id)
+        {
+            Id = id;
+        }
+    }
+}
