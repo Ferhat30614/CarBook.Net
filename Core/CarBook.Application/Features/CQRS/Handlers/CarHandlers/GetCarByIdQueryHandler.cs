6f84e33c06d71e0deabda1f41abf2c17e0776b25
Ferhat30614 <149321307+Ferhat30614@users.noreply.github.com>
2025-02-25 15:15:49 +0400

    Car için gerekli handler sınıfları tamam sıra apide

---
+++ Core/CarBook.Application/Features/CQRS/Handlers/CarHandlers/GetCarByIdQueryHandler.cs
@@ -0,0 +1,44 @@
+﻿using CarBook.Application.Features.CQRS.Queries.CarQueries;
+using CarBook.Application.Features.CQRS.Queries.CatQueries;
+using CarBook.Application.Features.CQRS.Results.CarResults;
+using CarBook.Application.Interfaces;
+using CarBook.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Application.Features.CQRS.Handlers.CarHandlers
+{
+    public class GetCarByIdQueryHandler
+    {
+
+        private readonly IRepository<Car> _repository;
+
+        public GetCarByIdQueryHandler(IRepository<Car> repository)
+        {
+            _repository = repository;
+        }
+        public async Task<GetCarQueryResult> Handle(GetCarByIdQuery query)
+        {
+
+            var values = await _repository.GetByIdAsync(query.Id);
+            return new GetCarQueryResult
+            {
+
+                CarID = values.CarID,
+                BrandID = values.BrandID,
+                Model = values.Model,
+                CoverImageUrl = values.CoverImageUrl,
+                Km = values.Km,
+                Transmission = values.Transmission,
+                Seat = values.Seat,
+                Luggage = values.Luggage,
+                Fuel = values.Fuel,
+                BigImageUrl = values.BigImageUrl
+
+            };
+        }
+    }
+}
