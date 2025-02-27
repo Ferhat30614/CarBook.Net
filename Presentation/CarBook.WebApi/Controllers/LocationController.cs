973fe9fe1ad8ab5c45a65effbfeee414a0153dcd
Ferhat30614 <149321307+Ferhat30614@users.noreply.github.com>
2025-02-27 19:59:05 +0400

    Location için gerekli işlemleri yaptımm amam bi yerdee bir hata var

---
+++ Presentation/CarBook.WebApi/Controllers/LocationController.cs
@@ -0,0 +1,53 @@
+﻿using CarBook.Application.Features.Mediator.Commands.LocationCommands;
+using CarBook.Application.Features.Mediator.Queries.LocationQueries;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarBook.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LocationController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public LocationController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> LocationList() 
+        {
+            var values = await _mediator.Send(new GetLocationQuery());
+            return Ok(values);        
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetLocation(int id)
+        {
+            var values = _mediator.Send(new GetLocationByIdQuery(id));
+            return Ok(values);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateLocation(CreateLocationCommand command)
+        {
+            var values = _mediator.Send(command);
+            return Ok("Konum eklendi");
+        }
+        [HttpPut]
+        public async Task<IActionResult> UpdateLocation(UpdateLocationCommand command)
+        {
+            var values = _mediator.Send(command);
+            return Ok("Konum Güncellendi");
+        
+        }
+        [HttpDelete]
+        public async Task<IActionResult> RemoveLocation(int id)
+        {
+            var values = _mediator.Send(new RemoveLocationCommand(id));
+            return Ok("Konum silindi");
+        }
+
+    }
+}
