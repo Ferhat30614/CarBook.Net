aa42b5085507a182cc3a7baf7bc0b823032cb995
Ferhat30614 <149321307+Ferhat30614@users.noreply.github.com>
2025-02-28 16:15:31 +0400

    Dto class library ve apiyi consume için düzenlemeler yaptım

--- Frontends/CarBook.WebUI/ViewComponents/AboutViewComponents/_AboutUsComponentPartial.cs
+++ Frontends/CarBook.WebUI/ViewComponents/AboutViewComponents/_AboutUsComponentPartial.cs
@@ -1,11 +1,28 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CarBook.Dto.AboutDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace CarBook.WebUI.ViewComponents.AboutViewComponents
 {
     public class _AboutUsComponentPartial:ViewComponent
     {
-        public IViewComponentResult Invoke() 
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _AboutUsComponentPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> Invoke() 
+        {
+            var client=_httpClientFactory.CreateClient();
+            var responseMessage =await client.GetAsync("https://localhost:7192/api/Abouts");
+            if (responseMessage.IsSuccessStatusCode) 
+            {
+                var dataJson = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultAboutDto>>(dataJson);
+            
+            }
             return View();
         }
     }
