3fdb35fb69882d1b6bdfceb2466c5ebdc3c138e4
Ferhat30614 <149321307+Ferhat30614@users.noreply.github.com>
2025-03-01 23:09:28 +0400

    Contact api işlemleri

--- Frontends/CarBook.WebUI/Controllers/ContactController.cs
+++ Frontends/CarBook.WebUI/Controllers/ContactController.cs
@@ -1,11 +1,38 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CarBook.Dto.ContactDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
 
 namespace CarBook.WebUI.Controllers
 {
     public class ContactController : Controller
     {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public ContactController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [HttpGet]
         public IActionResult Index()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(CreateContactDto createContactDto)
+        {
+            var client=_httpClientFactory.CreateClient();
+            createContactDto.SendDate = DateTime.Now;
+            var jsonData = JsonConvert.SerializeObject(createContactDto);
+            StringContent content = new StringContent(jsonData,Encoding.UTF8,"application/json");
+            var responseMessage =await client.PostAsync("https://localhost:7192/api/Contacts", content);
+            if(responseMessage.IsSuccessStatusCode)
+            {
+                RedirectToAction("Index","Default");
+
+            }
             return View();
         }
     }
