ee71130548b7e0938da02347e3012eb3df8ef3bc
Ferhat30614 <149321307+Ferhat30614@users.noreply.github.com>
2025-03-02 01:39:26 +0400

    GetLast5CarWithBrands işlemin,n hem bachand hemde frontend işlemi sorunsuz bir şekilde çalışıyor...

---
+++ Frontends/CarBook.WebUI/ViewComponents/DefaultViewComponents/_DefaultLast5CarsWithBrandsComponentPartial.cs
@@ -0,0 +1,31 @@
+﻿using CarBook.Dto.BannerDtos;
+using CarBook.Dto.CarDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace CarBook.WebUI.ViewComponents.DefaultViewComponents
+{
+    public class _DefaultLast5CarsWithBrandsComponentPartial:ViewComponent
+    {
+
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _DefaultLast5CarsWithBrandsComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7192/api/Cars/GetLast5CarWithBrand");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var dataJson = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultLast5CarWithBrandsDto>>(dataJson);
+                return View(values);
+            }
+            return View();
+        }
+
+    }
+}
