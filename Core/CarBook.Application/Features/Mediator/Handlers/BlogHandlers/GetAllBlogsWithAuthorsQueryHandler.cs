737e8645ccfcd58c2d54d6a8b2743aac050cb960
Ferhat30614 <149321307+Ferhat30614@users.noreply.github.com>
2025-03-04 14:13:24 +0400

    GetAllBlogsWithAuthors   tarafını backhande olusturdum sıra api tarafıda

---
+++ Core/CarBook.Application/Features/Mediator/Handlers/BlogHandlers/GetAllBlogsWithAuthorsQueryHandler.cs
@@ -0,0 +1,40 @@
+﻿using CarBook.Application.Features.Mediator.Queries.BlogQueries;
+using CarBook.Application.Features.Mediator.Results.BlogResults;
+using CarBook.Application.Interfaces.BlogInterfaces;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Application.Features.Mediator.Handlers.BlogHandlers
+{
+    public class GetAllBlogsWithAuthorsQueryHandler : IRequestHandler<GetAllBlogsWithAuthorsQuery, List<GetAllBlogsWithAuthorsQueryResult>>
+    {
+        private readonly IBlogRepository _repository;
+
+        public GetAllBlogsWithAuthorsQueryHandler(IBlogRepository repository)
+        {
+            _repository = repository;
+        }
+        public async Task<List<GetAllBlogsWithAuthorsQueryResult>> Handle(GetAllBlogsWithAuthorsQuery request, CancellationToken cancellationToken)
+        {
+
+            var values = _repository.GetAllBlogsWithAuthors();
+
+            return values.Select(x => new GetAllBlogsWithAuthorsQueryResult
+            {
+                BlogID = x.BlogID,
+                AuthorID = x.AuthorID,
+                AuthorName = x.Author.Name,
+                CreatedDate = x.CreatedDate,
+                CategoryID = x.CategoryID,   
+                Title = x.Title,
+                CoverImage = x.CoverImage,
+
+
+            }).ToList();
+        }
+    }
+}
