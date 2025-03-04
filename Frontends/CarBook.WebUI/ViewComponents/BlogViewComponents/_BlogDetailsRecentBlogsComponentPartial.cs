ab73a60e2b279a070fd66c1803169d7c45df8db8
Ferhat30614 <149321307+Ferhat30614@users.noreply.github.com>
2025-03-04 20:40:21 +0400

    Veritabanına TagCloud tablosunu ekledim...

--- Frontends/CarBook.WebUI/ViewComponents/BlogViewComponents/_BlogDetailsRecentBlogsComponentPartial.cs
+++ Frontends/CarBook.WebUI/ViewComponents/BlogViewComponents/_BlogDetailsRecentBlogsComponentPartial.cs
@@ -7,8 +7,24 @@ namespace CarBook.WebUI.ViewComponents.BlogViewComponents
 {
     public class _BlogDetailsRecentBlogsComponentPartial:ViewComponent
     {
-        public IViewComponentResult Invoke()
-        {            
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _BlogDetailsRecentBlogsComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7192/api/Blogs/GetLast3BlogsWithAuthorsList");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var dataJson = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultLast3BlogsWithAuthors>>(dataJson);
+                return View(values);
+
+            }
             return View();
         }
     }
