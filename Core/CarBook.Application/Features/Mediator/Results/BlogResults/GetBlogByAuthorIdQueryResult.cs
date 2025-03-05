ac25e6fe245883e1427af15333114e748c2dec03
Ferhat30614 <149321307+Ferhat30614@users.noreply.github.com>
2025-03-05 21:35:33 +0400

    Api consume işlemi tamam ve herşey sorunsuz çalışıyor...

--- Core/CarBook.Application/Features/Mediator/Results/BlogResults/GetBlogByAuthorIdQueryResult.cs
+++ Core/CarBook.Application/Features/Mediator/Results/BlogResults/GetBlogByAuthorIdQueryResult.cs
@@ -6,14 +6,15 @@ using System.Threading.Tasks;
 
 namespace CarBook.Application.Features.Mediator.Results.BlogResults
 {
-    public class GetBlogByAuthorIdQueryResul
+    public class GetBlogByAuthorIdQueryResult
+    {
         public int BlogID { get; set; }
         public int AuthorID { get; set; }       
         public string AuthorName{ get; set; }
         public string AuthorImageUrl{ get; set; }
         public string AuthorDescription{ get; set; }
-t
-    {
+
+ 
        
     }
 }
--- Frontends/CarBook.WebUI/ViewComponents/BlogViewComponents/_BlogDetailAuthorAboutComponentPartial.cs
+++ Frontends/CarBook.WebUI/ViewComponents/BlogViewComponents/_BlogDetailAuthorAboutComponentPartial.cs
@@ -1,11 +1,30 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CarBook.Dto.AuthorDtos;
+using CarBook.Dto.TagCloudDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace CarBook.WebUI.ViewComponents.BlogViewComponents
 {
     public class _BlogDetailAuthorAboutComponentPartial:ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _BlogDetailAuthorAboutComponentPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync($"https://localhost:7192/api/Blogs/GetBlogByAuthorId?id={id}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var dataJson = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<GetAuthorByBlogAuthorIdDto>(dataJson);
+                return View(values);
+
+            }
             return View();
         }
     }
