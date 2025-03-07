f8b87ad7349b08d82e67ef8b073d3dfae17f46df
Ferhat30614 <149321307+Ferhat30614@users.noreply.github.com>
2025-03-08 02:31:05 +0400

    Dropdown sorunsuz çalışıyor ve sıra araç ekleme sayfasında

--- CarBook.Dto/CarDtos/CreateCarDto.cs
+++ CarBook.Dto/CarDtos/CreateCarDto.cs
@@ -7,11 +7,9 @@ using System.Threading.Tasks;
 namespace CarBook.Dto.CarDtos
 {
     public class CreateCarDto
-    {
-        public int CarID { get; set; }
-        public int BrandID { get; set; }
+    {      
+        public int BrandID { get; set; }     
         public string Model { get; set; }
-        public string BrandName { get; set; }
         public string CoverImageUrl { get; set; }
         public int Km { get; set; }
         public string Transmission { get; set; }
@@ -19,5 +17,6 @@ namespace CarBook.Dto.CarDtos
         public byte Luggage { get; set; }
         public string Fuel { get; set; }
         public string BigImageUrl { get; set; }
+   
     }
 }
