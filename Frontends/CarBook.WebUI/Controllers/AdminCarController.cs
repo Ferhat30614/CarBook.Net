3501d974214c0680c876f05a56d96f18bbfe540b
Ferhat30614 <149321307+Ferhat30614@users.noreply.github.com>
2025-03-08 02:04:39 +0400

    Car üzerinde çalışmaya devam admin tarafında

--- Frontends/CarBook.WebUI/Controllers/AdminCarController.cs
+++ Frontends/CarBook.WebUI/Controllers/AdminCarController.cs
@@ -1,6 +1,8 @@
 ﻿using CarBook.Dto.BlogDtos;
+using CarBook.Dto.BrandDtos;
 using CarBook.Dto.CarDtos;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Newtonsoft.Json;
 
 namespace CarBook.WebUI.Controllers
@@ -28,9 +30,24 @@ namespace CarBook.WebUI.Controllers
 
 
         [HttpGet]
-        public IActionResult CreateCar()
-        {            
+        public async Task<IActionResult> CreateCar()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7192/api/Brands");           
+            var dataJson = await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<ResultBrandDto>>(dataJson);
+
+            List<SelectListItem> brandValues = (from x in values
+                                                select new SelectListItem
+                                                {
+                                                    Text = x.Name,
+                                                    Value=x.BrandID.ToString()    
+                                                }).ToList();    
+            
+            ViewBag.BrandValues = brandValues;   
+
             return View();
+        
         }
     }
 }
