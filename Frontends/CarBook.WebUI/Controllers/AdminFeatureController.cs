b9015be834450f26933b079cbf9986ba43063ca3
Ferhat30614 <149321307+Ferhat30614@users.noreply.github.com>
2025-03-09 01:43:45 +0400

    Featurre tarafı olusturuldu ve admin tarafında gerekli özellikler eklendi

---
+++ Frontends/CarBook.WebUI/Controllers/AdminFeatureController.cs
@@ -0,0 +1,108 @@
+﻿using CarBook.Dto.BrandDtos;
+using CarBook.Dto.CarDtos;
+using CarBook.Dto.FeatureDtos;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace CarBook.WebUI.Controllers
+{
+    public class AdminFeatureController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public AdminFeatureController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7192/api/Features");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var dataJson = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultFeatureDto>>(dataJson);
+                return View(values);
+            }
+            return View();
+        }
+
+
+
+        [HttpGet]
+        public  IActionResult CreateFeature()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> CreateFeature(CreateFeatureDto createFeatureDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createFeatureDto);
+            StringContent content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("https://localhost:7192/api/Features", content);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+
+            }
+            return View();
+        }
+
+        public async Task<IActionResult> RemoveFeature(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.DeleteAsync($"https://localhost:7192/api/Features?id={id}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+
+
+
+        [HttpGet]
+        public async Task<IActionResult> UpdateFeature(int id)
+        {          
+
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7192/api/Features/" + id);
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var dataJson = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<UpdateFeatureDto>(dataJson);
+                return View(values);
+            }
+
+            return View();
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateFeature(UpdateFeatureDto updateFeatureDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var datajson = JsonConvert.SerializeObject(updateFeatureDto);
+            StringContent stringContent = new StringContent(datajson, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PutAsync("https://localhost:7192/api/Features", stringContent);
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+
+
+
+    }
+}
