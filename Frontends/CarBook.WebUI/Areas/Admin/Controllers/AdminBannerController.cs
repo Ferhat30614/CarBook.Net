ba076147b2a1b714640a138288f31b9620ebe979
Ferhat30614 <149321307+Ferhat30614@users.noreply.github.com>
2025-03-09 12:50:32 +0400

    Area tarafında ben bannerlarımı listeledim...

---
+++ Frontends/CarBook.WebUI/Areas/Admin/Controllers/AdminBannerController.cs
@@ -0,0 +1,30 @@
+﻿using CarBook.Dto.BannerDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace CarBook.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class AdminBannerController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public AdminBannerController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7192/api/Banners");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var dataJson = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultBannerDto>>(dataJson);
+                return View(values);
+            }
+            return View();
+        }
+
+    }
+}
