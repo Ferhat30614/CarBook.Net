f21b063bf356126a67a14f5815a1cb9c0ea986a0
Ferhat30614 <149321307+Ferhat30614@users.noreply.github.com>
2025-03-11 00:18:35 +0400

    Blog idye göre yorumları listeleme işlemi aadmin tarafında tamam

---
+++ Frontends/CarBook.WebUI/Areas/Admin/Controllers/AdminCommentController.cs
@@ -0,0 +1,33 @@
+﻿using CarBook.Dto.CommentDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace CarBook.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("Admin/AdminComment")]
+    public class AdminCommentController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public AdminCommentController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [Route("Index/{id}")]
+        public async Task<IActionResult> Index(int id)  // Bu sayfada BlogIdye göre Yorumlar listeleniyor...
+        {
+            ViewBag.v=id;
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7192/api/Comments/GetCommentsByBlog?id=" +id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var dataJson = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultCommentDto>>(dataJson);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
