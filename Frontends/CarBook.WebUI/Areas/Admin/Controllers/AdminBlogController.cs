d5f8b1e62eb82ca6866e010f0d70aad662069465
Ferhat30614 <149321307+Ferhat30614@users.noreply.github.com>
2025-03-10 22:36:58 +0400

    AdminBlogdsa gerekli değişimler

--- Frontends/CarBook.WebUI/Areas/Admin/Controllers/AdminBlogController.cs
+++ Frontends/CarBook.WebUI/Areas/Admin/Controllers/AdminBlogController.cs
@@ -45,42 +45,6 @@ namespace CarBook.WebUI.Areas.Admin.Controllers
         }
 
 
-        [Route("UpdateBlog/{id}")]
-        [HttpGet]
-        public async Task<IActionResult> UpdateBlog(int id)
-        {
-
-            var client = _httpClientFactory.CreateClient();
-            var responseMessage = await client.GetAsync("https://localhost:7192/api/Blogs/" + id);
-
-            if (responseMessage.IsSuccessStatusCode)
-            {
-                var dataJson = await responseMessage.Content.ReadAsStringAsync();
-                var values = JsonConvert.DeserializeObject<UpdateBlogDto>(dataJson);
-                return View(values);
-            }
-
-            return View();
-        }
-
-
-        [Route("UpdateBlog/{id}")]
-        [HttpPost]
-        public async Task<IActionResult> UpdateBlog(UpdateBlogDto updateBlogDto)
-        {
-            var client = _httpClientFactory.CreateClient();
-            var datajson = JsonConvert.SerializeObject(updateBlogDto);
-            StringContent stringContent = new StringContent(datajson, Encoding.UTF8, "application/json");
-            var responseMessage = await client.PutAsync("https://localhost:7192/api/Blogs", stringContent);
-
-            if (responseMessage.IsSuccessStatusCode)
-            {
-                return RedirectToAction("Index", "AdminBlog", new { area = "Admin" });
-            }
-
-            return View();
-        }
-
 
     }
 }
