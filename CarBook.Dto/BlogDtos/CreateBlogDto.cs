057a9c416d18f069e4f908458b462f10d9b211d3
Ferhat30614 <149321307+Ferhat30614@users.noreply.github.com>
2025-03-10 22:30:56 +0400

    AdmiinBlog tarafını oluşturuyorum....

---
+++ CarBook.Dto/BlogDtos/CreateBlogDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Dto.BlogDtos
+{
+    public class CreateBlogDto
+    {
+        public int AuthorID { get; set; }
+        public int CategoryID { get; set; }
+        public string Title { get; set; }
+        public string AuthorName { get; set; }
+        public object CategoryName { get; set; }
+        public string CoverImage { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedDate { get; set; }
+
+    }
+}
---
+++ Frontends/CarBook.WebUI/Areas/Admin/Controllers/AdminBlogController.cs
@@ -0,0 +1,86 @@
+﻿using CarBook.Dto.BlogDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace CarBook.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("Admin/AdminBlog")]
+    public class AdminBlogController : Controller
+    {
+
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public AdminBlogController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [Route("Index")]
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7192/api/Blogs/GetAllBlogsWithAuthorsList");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var dataJson = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultAllBlogsWithAuthorsDto>>(dataJson);
+                return View(values);
+            }
+            return View();
+        }
+
+
+        [Route("RemoveBlog/{id}")]
+        public async Task<IActionResult> RemoveBlog(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.DeleteAsync($"https://localhost:7192/api/Blogs?id={id}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "AdminBlog", new { area = "Admin" });
+            }
+            return View();
+        }
+
+
+        [Route("UpdateBlog/{id}")]
+        [HttpGet]
+        public async Task<IActionResult> UpdateBlog(int id)
+        {
+
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7192/api/Blogs/" + id);
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var dataJson = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<UpdateBlogDto>(dataJson);
+                return View(values);
+            }
+
+            return View();
+        }
+
+
+        [Route("UpdateBlog/{id}")]
+        [HttpPost]
+        public async Task<IActionResult> UpdateBlog(UpdateBlogDto updateBlogDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var datajson = JsonConvert.SerializeObject(updateBlogDto);
+            StringContent stringContent = new StringContent(datajson, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PutAsync("https://localhost:7192/api/Blogs", stringContent);
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "AdminBlog", new { area = "Admin" });
+            }
+
+            return View();
+        }
+
+
+    }
+}
