cff4cbc69c1ac59bb62a07dc499e8b579d860329
Ferhat30614 <149321307+Ferhat30614@users.noreply.github.com>
2025-03-13 01:29:11 +0400

    Contact mesajlarını getir sayfasını ekledim sorunsuz çalışıyor....

---
+++ Frontends/CarBook.WebUI/Areas/Admin/Controllers/AdminContactController.cs
@@ -0,0 +1,32 @@
+﻿using CarBook.Dto.ContactDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace CarBook.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("Admin/AdminContact")]
+    public class AdminContactController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public AdminContactController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [Route("Index")]
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7192/api/Contacts");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var dataJson = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultContactDto>>(dataJson);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
