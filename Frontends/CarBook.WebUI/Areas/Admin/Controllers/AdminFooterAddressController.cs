a9f31f9eee2678b497af582a7aa94f3eff6c0a18
Ferhat30614 <149321307+Ferhat30614@users.noreply.github.com>
2025-03-13 01:55:42 +0400

    Footeraddress için gerekli eklemelere başladım...i

---
+++ Frontends/CarBook.WebUI/Areas/Admin/Controllers/AdminFooterAddressController.cs
@@ -0,0 +1,109 @@
+﻿using CarBook.Dto.FooterAddress;
+using CarBook.Dto.FooterAddressDtos;
+using CarBook.Dto.FooterAddressDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace CarBook.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("Admin/AdminFooterAddress")]
+    public class AdminFooterAddressController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public AdminFooterAddressController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [Route("Index")]
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7192/api/FooterAddresss");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var dataJson = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultFooterAddressDto>>(dataJson);
+                return View(values);
+            }
+            return View();
+        }
+
+        [Route("CreateFooterAddress")]
+        [HttpGet]
+        public IActionResult CreateFooterAddress()
+        {
+            return View();
+        }
+
+        [Route("CreateFooterAddress")]
+        [HttpPost]
+        public async Task<IActionResult> CreateFooterAddress(CreateFooterAddressDto createFooterAddressDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createFooterAddressDto);
+            StringContent content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("https://localhost:7192/api/FooterAddresss", content);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "AdminFooterAddress", new { area = "Admin" });
+
+            }
+            return View();
+        }
+
+
+        [Route("RemoveFooterAddress/{id}")]
+        public async Task<IActionResult> RemoveFooterAddress(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.DeleteAsync($"https://localhost:7192/api/FooterAddresss?id={id}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "AdminFooterAddress", new { area = "Admin" });
+            }
+            return View();
+        }
+
+
+        [Route("UpdateFooterAddress/{id}")]
+        [HttpGet]
+        public async Task<IActionResult> UpdateFooterAddress(int id)
+        {
+
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7192/api/FooterAddresss/" + id);
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var dataJson = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<UpdateFooterAddressDto>(dataJson);
+                return View(values);
+            }
+
+            return View();
+        }
+
+
+        [Route("UpdateFooterAddress/{id}")]
+        [HttpPost]
+        public async Task<IActionResult> UpdateFooterAddress(UpdateFooterAddressDto updateFooterAddressDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var datajson = JsonConvert.SerializeObject(updateFooterAddressDto);
+            StringContent stringContent = new StringContent(datajson, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PutAsync("https://localhost:7192/api/FooterAddresss", stringContent);
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "AdminFooterAddress", new { area = "Admin" });
+            }
+
+            return View();
+        }
+
+    }
+}
