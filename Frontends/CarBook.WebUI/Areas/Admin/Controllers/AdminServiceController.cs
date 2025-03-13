3fc4d0edd51dbb05e68469b27487e5f9b910a965
Ferhat30614 <149321307+Ferhat30614@users.noreply.github.com>
2025-03-13 16:17:28 +0400

    AdminService  için controller tarafı hazır

---
+++ Frontends/CarBook.WebUI/Areas/Admin/Controllers/AdminServiceController.cs
@@ -0,0 +1,107 @@
+﻿using CarBook.Dto.ServiceDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace CarBook.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("Admin/AdminService")]
+    public class AdminServiceController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public AdminServiceController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [Route("Index")]
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7192/api/Services");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var dataJson = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultServiceDto>>(dataJson);
+                return View(values);
+            }
+            return View();
+        }
+
+        [Route("CreateService")]
+        [HttpGet]
+        public IActionResult CreateService()
+        {
+            return View();
+        }
+
+        [Route("CreateService")]
+        [HttpPost]
+        public async Task<IActionResult> CreateService(CreateServiceDto createServiceDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createServiceDto);
+            StringContent content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("https://localhost:7192/api/Services", content);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "AdminService", new { area = "Admin" });
+
+            }
+            return View();
+        }
+
+
+        [Route("RemoveService/{id}")]
+        public async Task<IActionResult> RemoveService(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.DeleteAsync($"https://localhost:7192/api/Services?id={id}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "AdminService", new { area = "Admin" });
+            }
+            return View();
+        }
+
+
+        [Route("UpdateService/{id}")]
+        [HttpGet]
+        public async Task<IActionResult> UpdateService(int id)
+        {
+
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7192/api/Services/" + id);
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var dataJson = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<UpdateServiceDto>(dataJson);
+                return View(values);
+            }
+
+            return View();
+        }
+
+
+        [Route("UpdateService/{id}")]
+        [HttpPost]
+        public async Task<IActionResult> UpdateService(UpdateServiceDto updateServiceDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var datajson = JsonConvert.SerializeObject(updateServiceDto);
+            StringContent stringContent = new StringContent(datajson, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PutAsync("https://localhost:7192/api/Services", stringContent);
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "AdminService", new { area = "Admin" });
+            }
+
+            return View();
+        }
+
+    }
+}
