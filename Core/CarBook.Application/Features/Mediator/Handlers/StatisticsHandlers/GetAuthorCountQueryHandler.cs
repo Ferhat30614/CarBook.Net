3aac0c80730caa1d442741264f56fef3fb6bf1ea
Ferhat30614 <149321307+Ferhat30614@users.noreply.github.com>
2025-03-15 03:08:18 +0400

    5 tane handler olusturdum ve şimdi bunları denicem..:;

---
+++ Core/CarBook.Application/Features/Mediator/Handlers/StatisticsHandlers/GetAuthorCountQueryHandler.cs
@@ -0,0 +1,32 @@
+﻿using CarBook.Application.Features.Mediator.Queries.StatisticsQueries;
+using CarBook.Application.Features.Mediator.Results.StatisticsResults;
+using CarBook.Application.Interfaces.CarInterfaces;
+using CarBook.Application.Interfaces.StatisticInterfaces;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Application.Features.Mediator.Handlers.StatisticsHandlers
+{
+    public class GetAuthorCountQueryHandler : IRequestHandler<GetAuthorCountQuery, GetAuthorCountQueryResult>
+    {
+        private readonly IStatisticRepository _repository;
+
+        public GetAuthorCountQueryHandler(IStatisticRepository statisticRepository)
+        {
+            _repository = statisticRepository;
+        }
+
+        public async Task<GetAuthorCountQueryResult> Handle(GetAuthorCountQuery request, CancellationToken cancellationToken)
+        {
+            var values = _repository.GetAuthorCount();
+            return new GetAuthorCountQueryResult
+            {
+                AuthorCount = values
+            };
+        }
+    }
+}
---
+++ Core/CarBook.Application/Features/Mediator/Handlers/StatisticsHandlers/GetBrandCountQueryHandler.cs
@@ -0,0 +1,31 @@
+﻿using CarBook.Application.Features.Mediator.Queries.StatisticsQueries;
+using CarBook.Application.Features.Mediator.Results.StatisticsResults;
+using CarBook.Application.Interfaces.StatisticInterfaces;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Application.Features.Mediator.Handlers.StatisticsHandlers
+{
+    public class GetBrandCountQueryHandler : IRequestHandler<GetBrandCountQuery, GetBrandCountQueryResult>
+    {
+        private readonly IStatisticRepository _repository;
+
+        public GetBrandCountQueryHandler(IStatisticRepository statisticRepository)
+        {
+            _repository = statisticRepository;
+        }
+
+        public async Task<GetBrandCountQueryResult> Handle(GetBrandCountQuery request, CancellationToken cancellationToken)
+        {
+            var values = _repository.GetBrandCount();
+            return new GetBrandCountQueryResult
+            {
+                BrandCount = values
+            };
+        }
+    }
+}
