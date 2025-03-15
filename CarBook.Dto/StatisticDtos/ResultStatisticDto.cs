43d24855a9349380e96557d423920e63e05d344f
Ferhat30614 <149321307+Ferhat30614@users.noreply.github.com>
2025-03-15 20:45:24 +0400

    günün son commiti Uı Trafaı istatistikler için bitti sayılır...

--- CarBook.Dto/StatisticDtos/ResultStatisticDto.cs
+++ CarBook.Dto/StatisticDtos/ResultStatisticDto.cs
@@ -8,10 +8,21 @@ namespace CarBook.Dto.StatisticDtos
 {
     public class ResultStatisticDto
     {
-        public int CarCount { get; set; }
-        public int LocationCount { get; set; }
-        public int AuthorCount { get; set; }
-        public int BlogCount { get; set; }
-        public int BrandCount { get; set; }
+        public int carCount { get; set; }
+        public int locationCount { get; set; }
+        public int authorCount { get; set; }
+        public int blogCount { get; set; }
+        public int brandCount { get; set; }
+        public decimal avgRentPriceForDaily { get; set; }
+        public decimal avgRentPriceForWeekly { get; set; }
+        public decimal avgRentPriceForMonthly { get; set; }
+        public int carCountByTranmissionIsAuto { get; set; }
+        public int carCountByKmSmallerThen1000 { get; set; }
+        public int carCountByFuelGasolineOrDiesel { get; set; }
+        public int carCountByFuelElectric { get; set; }
+        public string carBrandAndModelByRentPriceDailyMax { get; set; }
+        public string carBrandAndModelByRentPriceDailyMin { get; set; }
+        public string brandNameByMaxCar { get; set; }
+        public string blogTitleByMaxBlogComment { get; set; }
     }
 }
