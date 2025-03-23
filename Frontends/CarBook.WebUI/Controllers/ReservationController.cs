919566e92a47b3344e2653b23ce4cfd6a60aec67
Ferhat30614 <149321307+Ferhat30614@users.noreply.github.com>
2025-03-23 04:06:54 +0400

    Dropdownları ekledim reservation sayfama

--- Frontends/CarBook.WebUI/Controllers/ReservationController.cs
+++ Frontends/CarBook.WebUI/Controllers/ReservationController.cs
@@ -1,11 +1,39 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CarBook.Dto.LocationDtos;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Newtonsoft.Json;
 
 namespace CarBook.WebUI.Controllers
 {
     public class ReservationController : Controller
     {
-        public IActionResult Index()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public ReservationController(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IActionResult> Index()
+        {
+            ViewBag.v1 = "Araç Kiralama";
+            ViewBag.v2 = "Araç Rezervasyon Formu";
+
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7192/api/Locations");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var dataJson = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultLocationDto>>(dataJson);
+
+                List<SelectListItem> values2 = (from x in values
+                                                select new SelectListItem
+                                                {
+                                                    Text = x.Name,
+                                                    Value = x.LocationID.ToString()
+                                                }).ToList();
+                ViewBag.v = values2;
+            }
+
             return View();
         }
     }
