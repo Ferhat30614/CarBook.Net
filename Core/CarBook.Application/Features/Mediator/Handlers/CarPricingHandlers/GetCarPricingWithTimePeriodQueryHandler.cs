17fd0e884c196ed6454c61b0811772016db5a2ed
Ferhat30614 <149321307+Ferhat30614@users.noreply.github.com>
2025-03-24 18:17:50 +0400

    handler tarafıda tamam gibi

--- Core/CarBook.Application/Features/Mediator/Handlers/CarPricingHandlers/GetCarPricingWithTimePeriodQueryHandler.cs
+++ Core/CarBook.Application/Features/Mediator/Handlers/CarPricingHandlers/GetCarPricingWithTimePeriodQueryHandler.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿using CarBook.Application.Features.Mediator.Queries.BlogQueries;
+using CarBook.Application.Features.Mediator.Queries.CarPricingQueries;
+using CarBook.Application.Features.Mediator.Results.BlogResults;
+using CarBook.Application.Features.Mediator.Results.CarPricingResults;
+using CarBook.Application.Interfaces;
+using CarBook.Application.Interfaces.CarPricingInterfaces;
+using CarBook.Domain.Entities;
+using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +14,25 @@ using System.Threading.Tasks;
 
 namespace CarBook.Application.Features.Mediator.Handlers.CarPricingHandlers
 {
-    public class GetCarPricingWithTimePeriodQueryHandler
+    public class GetCarPricingWithTimePeriodQueryHandler : IRequestHandler<GetCarPricingWithTimePeriodQuery, List<GetCarPricingWithTimePeriodQueryResult>>
     {
+        private readonly ICarPricingRepository _carPricingRepository;
+
+        public GetCarPricingWithTimePeriodQueryHandler(ICarPricingRepository carPricingRepository)
+        {
+            _carPricingRepository = carPricingRepository;
+        }
+
+        public async Task<List<GetCarPricingWithTimePeriodQueryResult>> Handle(GetCarPricingWithTimePeriodQuery request, CancellationToken cancellationToken)
+        {
+            var values =  _carPricingRepository.GetCarPricingWithTimePeriod();
+            return values.Select(x => new GetCarPricingWithTimePeriodQueryResult
+            {
+                
+                DailyAmount = x.Amounts[0],
+                WeeklyAmount = x.Amounts[1],
+                MonthlyAmount = x.Amounts[2]
+            }).ToList();
+        }
     }
 }
