47ec0534fe3f2fbbbed8cc17dca71cca99abbafb
Ferhat30614 <149321307+Ferhat30614@users.noreply.github.com>
2025-03-24 18:22:08 +0400

    apiyi ekledim ve küçük bir hata aldım...

--- Presentation/CarBook.WebApi/Controllers/CarPricingsController.cs
+++ Presentation/CarBook.WebApi/Controllers/CarPricingsController.cs
@@ -1,5 +1,6 @@
 ﻿using CarBook.Application.Features.Mediator.Queries.CarPricingQueries;
 using CarBook.Application.Features.Mediator.Queries.LocationQueries;
+using CarBook.Application.Features.Mediator.Results.CarPricingResults;
 using MediatR;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -20,6 +21,14 @@ namespace CarBook.WebApi.Controllers
         public async Task<IActionResult> CarPricingWithCarList()
         {
             var values = await _mediator.Send(new GetCarPricingWithCarQuery());
+            return Ok(values);
+
+        }
+
+        [HttpGet("CarPricingWithTimePeriodList")]
+        public async Task<IActionResult> CarPricingWithTimePeriodList()
+        {
+            var values = await _mediator.Send(new GetCarPricingWithTimePeriodQuery());
             return Ok(values);
 
         }
