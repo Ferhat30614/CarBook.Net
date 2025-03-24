af5d0970624fac16e777acc56d307f91da555951
Ferhat30614 <149321307+Ferhat30614@users.noreply.github.com>
2025-03-25 03:48:55 +0400

    herşey sorunsuz çalışıyor istatistikler yükleniyor....

--- Frontends/CarBook.WebUI/ViewComponents/DefaultViewComponents/_DefaultStatisticsComponentPartial.cs
+++ Frontends/CarBook.WebUI/ViewComponents/DefaultViewComponents/_DefaultStatisticsComponentPartial.cs
@@ -26,6 +26,37 @@ namespace CarBook.WebUI.ViewComponents.DefaultViewComponents
                 ViewBag.CarCount = values1.carCount;
             }
             #endregion
+
+            #region İstatistik2
+            var responseMessage2 = await client.GetAsync("https://localhost:7192/api/Statistics/GetLocationCount");
+            if (responseMessage2.IsSuccessStatusCode)
+            {
+                var jsonData2 = await responseMessage2.Content.ReadAsStringAsync();
+                var values2 = JsonConvert.DeserializeObject<ResultStatisticDto>(jsonData2);
+                ViewBag.LocationCount = values2.locationCount;
+            }
+            #endregion
+
+            #region İstatistik3
+            var responseMessage3 = await client.GetAsync("https://localhost:7192/api/Statistics/GetBrandCount");
+            if (responseMessage3.IsSuccessStatusCode)
+            {
+                var jsonData3 = await responseMessage3.Content.ReadAsStringAsync();
+                var values3 = JsonConvert.DeserializeObject<ResultStatisticDto>(jsonData3);
+                ViewBag.BrandCount = values3.brandCount;
+            }
+            #endregion
+
+            #region İstatistik4
+            var responseMessage4 = await client.GetAsync("https://localhost:7192/api/Statistics/GetCarCountByFuelElectric");
+            if (responseMessage4.IsSuccessStatusCode)
+            {
+                var jsonData4 = await responseMessage4.Content.ReadAsStringAsync();
+                var values4 = JsonConvert.DeserializeObject<ResultStatisticDto>(jsonData4);
+                ViewBag.CarCountByFuelElectric = values4.carCountByFuelElectric;
+            }
+            #endregion
+
             return View();      
         }
     }
