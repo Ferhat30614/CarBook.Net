9e0848c8669970d02c3790659405196090aafe24
Ferhat30614 <149321307+Ferhat30614@users.noreply.github.com>
2025-03-26 02:51:08 +0400

    bazı düzenlemeler ve tabiki footer tarafında alert eklemeleri

--- Frontends/CarBook.WebUI/ViewComponents/CommentViewComponents/_CommentListByBlogComponentPartial.cs
+++ Frontends/CarBook.WebUI/ViewComponents/CommentViewComponents/_CommentListByBlogComponentPartial.cs
@@ -1,11 +1,28 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CarBook.Dto.CommentDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace CarBook.WebUI.ViewComponents.CommentViewComponents
 {
     public class _CommentListByBlogComponentPartial:ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _CommentListByBlogComponentPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IViewComponentResult> InvokeAsync(int id)
+        {
+            ViewBag.v = id;
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7192/api/Comments/GetCommentsByBlog?id=" + id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var dataJson = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultCommentDto>>(dataJson);
+                return View(values);
+            }
             return View();
         }
 
