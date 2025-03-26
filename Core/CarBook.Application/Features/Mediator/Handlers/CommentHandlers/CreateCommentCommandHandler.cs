a345afd8db9d39e527685283a1d074a5d7803639
Ferhat30614 <149321307+Ferhat30614@users.noreply.github.com>
2025-03-26 05:17:01 +0400

    CreateCommentCommand  için bachandişlemlerine devam ediyorum

---
+++ Core/CarBook.Application/Features/Mediator/Handlers/CommentHandlers/CreateCommentCommandHandler.cs
@@ -0,0 +1,33 @@
+﻿using CarBook.Application.Features.Mediator.Commands.CommentCommands;
+using CarBook.Application.Interfaces;
+using CarBook.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Application.Features.Mediator.Handlers.CommentHandlers
+{
+    public class CreateCommentCommandHandler : IRequestHandler<CreateCommentCommand>
+    {
+        private readonly IRepository<Comment> _commentRepository;
+
+        public CreateCommentCommandHandler(IRepository<Comment> commentRepository)
+        {
+            _commentRepository = commentRepository;
+        }
+
+        public async Task Handle(CreateCommentCommand request, CancellationToken cancellationToken)
+        {
+            await _commentRepository.CreateAsync(new Comment
+            {
+                Name=request.Name,
+                CreatedDate=request.CreatedDate,
+                BlogID=request.BlogID,  
+                Description=request.Description
+            });
+        }
+    }
+}
