66a1c0d6542a75f58a0d9ee9a237eb47128f240a
Ferhat30614 <149321307+Ferhat30614@users.noreply.github.com>
2025-03-27 18:53:38 +0400

    Gerekli eklemeleri yapıyorum repo için...

---
+++ Infrastructure/CarBook.Persistence/Repositories/CarFeatureRepositories/CarFeatureRepository.cs
@@ -0,0 +1,18 @@
+﻿using CarBook.Application.Interfaces.CarFeatureInterfaces;
+using CarBook.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Persistence.Repositories.CarFeatureRepositories
+{
+    public class CarFeatureRepository : ICarFeatureRepository
+    {
+        public List<CarFeature> GetCarFeaturesByCarId()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
