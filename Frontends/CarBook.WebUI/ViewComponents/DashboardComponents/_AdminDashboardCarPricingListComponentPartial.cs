5e3a78f520b7df8e614a517897ed16599a74574e
Ferhat30614 <149321307+Ferhat30614@users.noreply.github.com>
2025-04-03 02:51:19 +0400

    Arac kiralama ücretleri için dashboarda carpricingde kullandımız aynı apiyi kullandık ...sorunsuz çalışıyor...

---
+++ Frontends/CarBook.WebUI/ViewComponents/DashboardComponents/_AdminDashboardCarPricingListComponentPartial.cs
@@ -0,0 +1,33 @@
+﻿using CarBook.Dto.CarPricingDtos;
+using CarBook.Dto.StatisticDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace CarBook.WebUI.ViewComponents.DashboardComponents
+{
+    public class _AdminDashboardCarPricingListComponentPartial:ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _AdminDashboardCarPricingListComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            ViewBag.v1 = "Paketler";
+            ViewBag.v2 = "Araç Fiyat Paketleri";
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7192/api/CarPricings/CarPricingWithTimePeriodList");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var dataJson = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultCarPricingListWithModelDto>>(dataJson);
+                return View(values);
+            }
+            return View();
+        }
+
+    }
+}
