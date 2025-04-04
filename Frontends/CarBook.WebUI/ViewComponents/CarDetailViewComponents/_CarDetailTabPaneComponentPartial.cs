a36c46e10c388f25df9970e194236f5b1f162aa7
Ferhat30614 <149321307+Ferhat30614@users.noreply.github.com>
2025-04-04 21:09:07 +0400

    Araç Özellikleri için düzenlmelere başladım herşey sorunsuz devam ediyor...

---
+++ Frontends/CarBook.WebUI/ViewComponents/CarDetailViewComponents/_CarDetailTabPaneComponentPartial.cs
@@ -0,0 +1,14 @@
+﻿using CarBook.Dto.CarDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace CarBook.WebUI.ViewComponents.CarDetailViewComponents
+{
+    public class _CarDetailTabPaneComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
