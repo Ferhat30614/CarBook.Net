0c6dc814d74505042aebf505d253cf6efe58a6b4
Ferhat30614 <149321307+Ferhat30614@users.noreply.github.com>
2025-04-23 02:31:27 +0400

    Geceniin son commiti ... Gönderilen username ve passworda göre role değeri dönen ,yoksada false dönen api yazdım....

--- Core/CarBook.Application/Features/Mediator/Handlers/AppUserHandlers/GetCheckAppUserQueryHandler.cs
+++ Core/CarBook.Application/Features/Mediator/Handlers/AppUserHandlers/GetCheckAppUserQueryHandler.cs
@@ -13,10 +13,34 @@ namespace CarBook.Application.Features.Mediator.Handlers.AppUserHandlers
 {
     public class GetCheckAppUserQueryHandler : IRequestHandler<GetCheckAppUserQuery, GetCheckAppUserQueryResult>
     {
-        
-        public Task<GetCheckAppUserQueryResult> Handle(GetCheckAppUserQuery request, CancellationToken cancellationToken)
+        private readonly IRepository<AppUser> _appUserRepository;   
+        private readonly IRepository<AppRole> _appRoleRepository;
+
+        public GetCheckAppUserQueryHandler(IRepository<AppUser> appUserRepository, IRepository<AppRole> appRoleRepository)
         {
-            throw new NotImplementedException();
+            _appUserRepository = appUserRepository;
+            _appRoleRepository = appRoleRepository;
+        }
+
+        public async Task<GetCheckAppUserQueryResult> Handle(GetCheckAppUserQuery request, CancellationToken cancellationToken)
+        {
+            var values = new GetCheckAppUserQueryResult();
+            var user = await _appUserRepository.GetByFilterAsync(x => x.UserName == request.UserName && x.Password == request.Password);
+            if (user == null)
+            {
+
+                values.IsExist = false;
+
+            }
+            else { 
+            
+                values.IsExist = true;  
+                values.UserName = user.UserName;
+                values.Role = (await _appRoleRepository.GetByIdAsync(user.AppRoleId)).AppRoleName;
+                values.Id = user.AppUserId;
+                
+            }
+            return values;  
         }
     }
 }
