8ad3ca33527fe3d2be225c8ad1fbe3dc83b70c84
Ferhat30614 <149321307+Ferhat30614@users.noreply.github.com>
2025-04-30 12:28:25 +0400

    SignOut işlemi

---
+++ Frontends/CarBook.WebUI/Controllers/SignOutController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarBook.WebUI.Controllers
+{
+    public class SignOutController : Controller
+    {
+        public IActionResult Index()
+        {
+            HttpContext.SignOutAsync();
+            return RedirectToAction("Index", "Default");
+        }
+    }
+}
