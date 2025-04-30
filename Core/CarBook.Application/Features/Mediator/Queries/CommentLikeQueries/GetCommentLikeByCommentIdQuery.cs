cb3b4befe2d0b5c6c1bcefd1f0b5a6b019c877ca
Ferhat30614 <149321307+Ferhat30614@users.noreply.github.com>
2025-04-30 23:45:40 +0400

    Commentlike için repo ve mediator işlemleri tamam ...

---
+++ Core/CarBook.Application/Features/Mediator/Queries/CommentLikeQueries/GetCommentLikeByCommentIdQuery.cs
@@ -0,0 +1,22 @@
+﻿using CarBook.Application.Features.Mediator.Results.CommentLikeResults;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Application.Features.Mediator.Queries.CommentLikeQueries
+{
+    public class GetCommentLikeByCommentIdQuery:IRequest<GetCommentLikeByCommentIdQueryResult>
+    {
+        public int CommentID { get; set; }
+        public int AppUserID { get; set; }
+
+        public GetCommentLikeByCommentIdQuery(int commentID, int appUserID)
+        {
+            CommentID = commentID;
+            AppUserID = appUserID;
+        }
+    }
+}
---
+++ Core/CarBook.Application/Features/Mediator/Results/CommentLikeResults/GetCommentLikeByCommentIdQueryResult.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Application.Features.Mediator.Results.CommentLikeResults
+{
+    public class GetCommentLikeByCommentIdQueryResult
+    {
+        public int LikeCount { get; set; }
+        public int DislikeCount { get; set; }
+        public int CommentID { get; set; }
+        public bool? UserVote { get; set; }
+    }
+}
