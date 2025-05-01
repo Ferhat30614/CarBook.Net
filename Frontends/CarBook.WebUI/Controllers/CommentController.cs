15859004c9d6b338567797743ea2fe4f2bcc8b2d
Ferhat30614 <149321307+Ferhat30614@users.noreply.github.com>
2025-05-01 15:47:46 +0400

    Herşey yolunda Yoruma yanıt verme ve bu verilen yanıtın veritabanına kaydedilmesi sorunsuz bir şekilde çalısıyor...

--- Frontends/CarBook.WebUI/Controllers/CommentController.cs
+++ Frontends/CarBook.WebUI/Controllers/CommentController.cs
@@ -1,12 +1,50 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CarBook.Dto.CommentDtos;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Text;
 
 namespace CarBook.WebUI.Controllers
 {
     public class CommentController : Controller
     {
-        public IActionResult Index()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public CommentController(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [Authorize]
+        public async  Task<IActionResult> AddReply(CreateReplyCommentDto createReplyCommentDto)
+        {
+            var claim =User.Claims.FirstOrDefault(a=>a.Type== "Username");
+            var claim2 =User.Claims.FirstOrDefault(a=>a.Type== "Eposta");
+            var Username = "";
+            var Eposta = "";
+            if (claim != null && claim2 != null )
+            { 
+
+                Username = claim.Value;
+                Eposta = claim2.Value; 
+            
+            }
+
+            createReplyCommentDto.Name = Username;
+            createReplyCommentDto.Email = Eposta;   
+
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createReplyCommentDto);
+            StringContent content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("https://localhost:7192/api/Comments/CreateComment", content);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("BlogDetails","Blog", new { id = createReplyCommentDto.BlogID });
+
+            }
             return View();
+       
         }
     }
 }
---
+++ Frontends/CarBook.WebUI/ViewComponents/CommentViewComponents/_CommentReplyComponentPartial.cs
@@ -0,0 +1,23 @@
+﻿using CarBook.Dto.CommentDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace CarBook.WebUI.ViewComponents.CommentViewComponents
+{
+    public class _CommentReplyComponentPartial : ViewComponent
+    {
+       
+        public async Task<IViewComponentResult> InvokeAsync(int commentId)
+        {
+            var value = new CreateReplyCommentDto
+            {
+                ParentCommentId = commentId,
+                BlogID = ViewBag.BlogID,
+            };
+
+
+            return View(value);
+        }
+
+    }
+}
