220aba0af272be4697e534c2d55084d424ef0ce9
Ferhat30614 <149321307+Ferhat30614@users.noreply.github.com>
2025-05-01 20:19:19 +0400

    Sorunlar yaşıyorum tam olarak istdeğim yapı bu değil

---
+++ Frontends/CarBook.WebUI/ViewComponents/CommentViewComponents/_CommentListResponseComponentPartial.cs
@@ -0,0 +1,30 @@
+﻿using CarBook.Dto.CommentDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace CarBook.WebUI.ViewComponents.CommentViewComponents
+{
+    public class _CommentListResponseComponentPartial : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _CommentListResponseComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IViewComponentResult> InvokeAsync(int commentId)
+        {
+            ViewBag.CommentId = commentId;
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7192/api/Comments/GetReplyCommentsByComment?id=" + commentId);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var dataJson = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultReplyCommentDto>>(dataJson);
+                return View(values);
+            }
+            return View();
+        }
+
+    }
+}
