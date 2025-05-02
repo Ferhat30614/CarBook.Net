fe7af34e4bf9b6c420ae1bbba8ab071caf8434ff
Ferhat30614 <149321307+Ferhat30614@users.noreply.github.com>
2025-05-02 22:54:32 +0400

    Frontend için tasarımmım şimdilik böyle olsun

---
+++ CarBook.Dto/MessageDtos/ResultMessageByCurrentUserDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Dto.MessageDtos
+{
+    public class ResultMessageByCurrentUserDto
+    {
+        public int MessageID { get; set; }
+        public int CurrentUserID { get; set; }
+        public int OtherUserID { get; set; }
+        public string OtherUserName { get; set; }
+    }
+}
---
+++ Frontends/CarBook.WebUI/Controllers/MessageController.cs
@@ -0,0 +1,37 @@
+﻿using CarBook.Dto.BlogDtos;
+using CarBook.Dto.MessageDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Security.Claims;
+
+namespace CarBook.WebUI.Controllers
+{
+    public class MessageController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public MessageController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var claim = User.Claims.FirstOrDefault(a => a.Type == ClaimTypes.NameIdentifier);
+            var userId = 0;
+
+            if (claim != null) {
+                userId = int.Parse(claim.Value);            
+            }
+
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7192/api/Messages/GetMessageByCurrentUser?currentUserId=" + userId);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var dataJson = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultMessageByCurrentUserDto>>(dataJson);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
